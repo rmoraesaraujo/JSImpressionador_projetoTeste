a43aba8bcb24140422e8ca52ac7d38b03fff3bcb
Renan Araujo <renan290812@gmail.com>
2024-05-24 06:57:08 -0300

    Atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import { turboLogger } from './turboLogger.js';
 
 console.log('Estou usando o Github');
-turboLogger('Estou aprendendo o fluxo do git');
+turboLogger('Estou aprendendo o fluxo do git', 'red');
+
+turboLogger(
+  'Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema',
+  'darkgreen'
+);
